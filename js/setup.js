de4ebcbd4cdd25382d53fd87874d054895d62fb7
ehassed3 <ehassed3@gmail.com>
2017-11-22 14:24:22 +0300

    Создает функцию задания случайного цвета и добавляет массивы цветов

--- js/setup.js
+++ js/setup.js
@@ -2,33 +2,39 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-var getRandomName = function(name, surname) {
+var getRandomName = function (name, surname) {
   return name[Math.floor(Math.random() * name.length)] + '' + surname[Math.floor(Math.random() * surname.length)];
+};
+
+var getRandomColor = function (color) {
+  return color[Math.floor(Math.random() * color.length)];
 };
 
 var wizards = [
   {
     name: getRandomName(WIZARD_NAMES, WIZARD_SURNAMES),
-    coatColor: getRandomCoatColor,
-    eyesColor: getRandomEyesColor
+    coatColor: getRandomColor(WIZARD_COAT_COLORS),
+    eyesColor: getRandomColor(WIZARD_EYES_COLORS)
   },
   {
     name: getRandomName(WIZARD_NAMES, WIZARD_SURNAMES),
-    coatColor: getRandomCoatColor,
-    eyesColor: getRandomEyesColor
+    coatColor: getRandomColor(WIZARD_COAT_COLORS),
+    eyesColor: getRandomColor(WIZARD_EYES_COLORS)
   },
   {
     name: getRandomName(WIZARD_NAMES, WIZARD_SURNAMES),
-    coatColor: getRandomCoatColor,
-    eyesColor: getRandomEyesColor
+    coatColor: getRandomColor(WIZARD_COAT_COLORS),
+    eyesColor: getRandomColor(WIZARD_EYES_COLORS)
   },
   {
     name: getRandomName(WIZARD_NAMES, WIZARD_SURNAMES),
-    coatColor: getRandomCoatColor,
-    eyesColor: getRandomEyesColor
+    coatColor: getRandomColor(WIZARD_COAT_COLORS),
+    eyesColor: getRandomColor(WIZARD_EYES_COLORS)
   }
 ];
