7d4525023cbe9c51f2cc924bb8fa69fd6c269a6a
ehassed3 <ehassed3@gmail.com>
2017-11-22 14:14:31 +0300

    Добавляет функцию генерации случайного имени персонажа

--- js/setup.js
+++ js/setup.js
@@ -1,26 +1,33 @@
 'use strict';
 
+var WIZARD_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
+var getRandomName = function(name, surname) {
+  return name[Math.floor(Math.random() * name.length)] + '' + surname[Math.floor(Math.random() * surname.length)];
+};
+
 var wizards = [
   {
-    name: getRandomName,
+    name: getRandomName(WIZARD_NAMES, WIZARD_SURNAMES),
     coatColor: getRandomCoatColor,
     eyesColor: getRandomEyesColor
   },
   {
-    name: getRandomName,
+    name: getRandomName(WIZARD_NAMES, WIZARD_SURNAMES),
     coatColor: getRandomCoatColor,
     eyesColor: getRandomEyesColor
   },
   {
-    name: getRandomName,
+    name: getRandomName(WIZARD_NAMES, WIZARD_SURNAMES),
     coatColor: getRandomCoatColor,
     eyesColor: getRandomEyesColor
   },
   {
-    name: getRandomName,
+    name: getRandomName(WIZARD_NAMES, WIZARD_SURNAMES),
     coatColor: getRandomCoatColor,
     eyesColor: getRandomEyesColor
   }
