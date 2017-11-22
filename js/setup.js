cf9e8ae3800b3e66d6a4b8db3a7032641a8ca5cc
ehassed3 <ehassed3@gmail.com>
2017-11-22 13:57:52 +0300

    Создает массив из 4 объектов, описывающих похожих персонажей

--- js/setup.js
+++ js/setup.js
@@ -2,3 +2,26 @@
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
+
+var wizards = [
+  {
+    name: getRandomName,
+    coatColor: getRandomCoatColor,
+    eyesColor: getRandomEyesColor
+  },
+  {
+    name: getRandomName,
+    coatColor: getRandomCoatColor,
+    eyesColor: getRandomEyesColor
+  },
+  {
+    name: getRandomName,
+    coatColor: getRandomCoatColor,
+    eyesColor: getRandomEyesColor
+  },
+  {
+    name: getRandomName,
+    coatColor: getRandomCoatColor,
+    eyesColor: getRandomEyesColor
+  }
+];
