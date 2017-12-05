679536eba276958d12910f20febb61d795041215
ehassed3 <ehassed3@gmail.com>
2017-12-05 16:46:43 +0300

    Добавляет реализацию перетаскивания диалога

--- js/dialog.js
+++ js/dialog.js
@@ -16,6 +16,7 @@
 
   var openPopup = function () {
     userDialog.classList.remove('hidden');
+    userDialog.removeAttribute('style');
     document.addEventListener('keydown', onPopupEscPress);
   };
 
@@ -62,4 +63,43 @@
 
   var userDialogForm = userDialog.querySelector('.setup-wizard-form');
   userDialogForm.setAttribute('action', 'https://js.dump.academy/code-and-magick');
+
+  var dialogHandle = userDialog.querySelector('.setup-user-pic');
+  dialogHandle.nextElementSibling.classList.add('hidden');
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+      userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 })();
